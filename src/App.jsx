04017e5a8d8c865f7431b800fdcdc49c85f7a223
Pradyumna-1 <pradyumnakumar35860@gmail.com>
2025-02-05 00:27:05 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header";
 import { AddContact } from "./Components/AddContact";
 import { ContactList } from "./Components/ContactList";
+import uuid4 from "uuid4";
 
 function App() {
-  const [contact, setContact] = useState([]);
+  const localStorageKey = "contact";
+  const [contact, setContact] = useState(() => {
+    return JSON.parse(localStorage.getItem(localStorageKey)) || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(contact));
+  }, [contact]);
+
   const addContact = (data) => {
-    setContact([...contact, data]);
+    setContact([...contact, { id: uuid4(), ...data }]);
   };
 
+  const removeContact = (id) => {
+    const updatedList = contact.filter((val) => {
+      return val.id !== id;
+    });
+    setContact(updatedList);
+  };
   return (
     <>
       <Header />
       <AddContact addContact={addContact} />
-      <ContactList contact={contact} />
+      <ContactList contact={contact} removeContact={removeContact} />
     </>
   );
 }
--- src/Components/ContactList.jsx
+++ src/Components/ContactList.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-
-export const ContactList = ({ contact }) => {
+import DeleteIcon from "@mui/icons-material/Delete";
+export const ContactList = (props) => {
+  const { contact, removeContact } = props;
   console.log(contact, "from contactList");
 
   return (
     <>
       <div className="contactsHeader">
-      {contact.map((val) => {
-        const { name, email, mobile } = val;
-        return (
-          <>
-            <div key={email} className="contacts">
-              <div>{name}</div>
-              <div>{email}</div>
-              <div>{mobile}</div>
-            </div>
-          </>
-        );
-      })}
+        {contact.map((val) => {
+          const { id, name, email, mobile } = val;
+          return (
+            <>
+              <div className="contacts">
+                <div>{name}</div>
+                <div>{email}</div>
+                <div>{mobile}</div>
+                <span onClick={() => removeContact(id)}>
+                  <DeleteIcon />
+                </span>
+              </div>
+            </>
+          );
+        })}
       </div>
     </>
   );
